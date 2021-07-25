32553f376f181ee503e53495adc02284d2a4b4c3
Juliana C.O. Leite <81474433+juhcavole@users.noreply.github.com>
2021-07-25 11:22:30 -0300

    Delete Variable.java

--- Variable.java
+++
@@ -1,9 +0,0 @@
-public class Variable {
-    public static void main(String[] args) {
-        String basket = "Apple";
-        System.out.println(basket);
-
-        basket = "Orange";
-        System.out.println(basket);
-    }
-}
