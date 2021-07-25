230a014a3ad0b930d623ae437793004ce452f1bc
Juliana C.O. Leite <81474433+juhcavole@users.noreply.github.com>
2021-07-25 10:34:13 -0300

    Delete Java apostila directory

--- Java apostila/MeuPrograma.java
+++
@@ -1,9 +0,0 @@
-
-
-class MeuPrograma {
-
-	public static void main(String[] args) {
-		System.out.println("Eu vou conseguir com a ajuda de Deus e meus amigos de Luz!!,\nAmo Programar!!" );
-		
-	}
-}
