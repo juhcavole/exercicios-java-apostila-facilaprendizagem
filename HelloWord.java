f2aadf87d5bc63b89fd01d4d726ee7d594fc9b3b
Juliana C.O. Leite <81474433+juhcavole@users.noreply.github.com>
2021-07-25 11:23:03 -0300

    Delete HelloWord.java

--- HelloWord.java
+++
@@ -1,8 +0,0 @@
-public class HelloWord {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
-        System.out.print("Good benning\t");
-        System.out.print("is the half of the sucess");
-    }
-}
